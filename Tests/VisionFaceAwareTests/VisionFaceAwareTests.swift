fe521631abb60455c739af7f37a9044119488dcb
Gentian Barileva <gentianbarileva@gmail.com>
2022-07-28 20:42:30 +0200

    Remove boilerplate tests.

--- Tests/VisionFaceAwareTests/VisionFaceAwareTests.swift
+++ Tests/VisionFaceAwareTests/VisionFaceAwareTests.swift
@@ -1,11 +1,4 @@
 import XCTest
 @testable import VisionFaceAware
 
-final class VisionFaceAwareTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(VisionFaceAware().text, "Hello, World!")
-    }
-}
+final class VisionFaceAwareTests: XCTestCase { }
